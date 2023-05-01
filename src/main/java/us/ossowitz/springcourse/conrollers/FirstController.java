76ef3a9b32868ab841ba31204f5efbd254a40e4c
Ossowitz <76myxomor76@gmail.com>
2023-05-01 18:02:16 +0300

    InitialCommit

--- src/main/java/us/ossowitz/springcourse/conrollers/FirstController.java
+++ src/main/java/us/ossowitz/springcourse/conrollers/FirstController.java
@@ -1,6 +1,7 @@
 package us.ossowitz.springcourse.conrollers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +12,9 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello, " + name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        model.addAttribute("message", "Hello, " + name + " " + surname);
 
         return "first/hello";
     }
