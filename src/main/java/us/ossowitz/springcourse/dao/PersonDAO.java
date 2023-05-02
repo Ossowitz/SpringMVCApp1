af6499eca40d2628003fa814d1bc3f8cc75230ae
Ossowitz <76myxomor76@gmail.com>
2023-05-02 19:09:12 +0300

    InitialCommit

---
+++ src/main/java/us/ossowitz/springcourse/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package us.ossowitz.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import us.ossowitz.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private List<Person> people;
+    private static int PEOPLE_COUNT;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> id == person.getId()).findAny().orElse(null);
+    }
+}
