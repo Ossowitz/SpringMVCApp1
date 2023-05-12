b8bc146b279560bbe114c840b84b325b7f0851ca
Ossowitz <76myxomor76@gmail.com>
2023-05-12 16:45:04 +0300

    InitialCommit

--- src/main/java/us/ossowitz/springcourse/dao/PersonDAO.java
+++ src/main/java/us/ossowitz/springcourse/dao/PersonDAO.java
@@ -3,21 +3,27 @@ package us.ossowitz.springcourse.dao;
 import org.springframework.stereotype.Component;
 import us.ossowitz.springcourse.models.Person;
 
-import java.util.ArrayList;
+import java.sql.Connection;
 import java.util.List;
 
 @Component
 public class PersonDAO {
-    private List<Person> people;
     private static int PEOPLE_COUNT;
 
-    {
-        people = new ArrayList<>();
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
 
-        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob", 52, "bob@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike", 18, "mike@yahoo.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy", 34, "katy@gmail.com"));
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     public List<Person> index() {
@@ -25,7 +31,7 @@ public class PersonDAO {
     }
 
     public Person show(int id) {
-        return people.stream().filter(person -> id == person.getId()).findAny().orElse(null);
+//        return people.stream().filter(person -> id == person.getId()).findAny().orElse(null);
     }
 
     public void save(Person person) {
@@ -34,11 +40,11 @@ public class PersonDAO {
     }
 
     public void update(int id, Person updatedPerson) {
-        Person personToBeUpdated = show(id);
-
-        personToBeUpdated.setName(updatedPerson.getName());
-        personToBeUpdated.setAge(updatedPerson.getAge());
-        personToBeUpdated.setEmail(updatedPerson.getEmail());
+//        Person personToBeUpdated = show(id);
+//
+//        personToBeUpdated.setName(updatedPerson.getName());
+//        personToBeUpdated.setAge(updatedPerson.getAge());
+//        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
 
     public void delete(int id) {
