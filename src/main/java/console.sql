3691cd8666aaa0aa90c891cd81f023f12a322348
Ossowitz <76myxomor76@gmail.com>
2023-05-19 19:44:05 +0300

    InitialCommit

--- src/main/java/console.sql
+++ src/main/java/console.sql
@@ -11,3 +11,19 @@ VALUES ('Tom', 18, 'tom@mail.ru'),
        ('Bob', 28, 'bob@mail.ru'),
        ('Bob1', 38, 'bob346@mail.ru'),
        ('Bob2', 45, 'bob34563467@mail.ru');
+
+SELECT P.name name,
+       O.item_name
+FROM Person AS P,
+     Order_ AS O
+WHERE P.user_id = O.order_id;
+
+SELECT P.name name,
+       O.item_name
+FROM Person P
+         JOIN Order_ O on P.user_id = O.user_id;
+
+SELECT *
+FROM Person AS P
+         CROSS JOIN
+     Order_ AS O;
